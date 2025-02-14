9dbbfabe5f870b5184040e62674d96c5042e02cc
David Roman Louis FRISCHER <frischer@swiftmail.cs.ucl.ac.uk>
2025-02-14 15:21:05 +0000

    saving workd

--- src/detect_wagon.py
+++ src/detect_wagon.py
@@ -2,29 +2,31 @@ from ultralytics import YOLO
 import cv2
 
 # Load YOLO model
-model = YOLO("./wagons_1302.pt")
+model = YOLO("../models/wagons_1302.pt")
 
-# Read the image in grayscale
-image_gray = cv2.imread("output_folder_cropped/cropped_49_77_crop_0_0.jpg", cv2.IMREAD_GRAYSCALE)
+def detect_wagon(image_path):
+    """Detect wagons in the given image."""
+    # Load the image
+    filename = image_path.split("/")[-1].split(".")[0]
+    image = cv2.imread(image_path, cv2.COLOR_GRAY2BGR)
+    if image is None:
+        raise FileNotFoundError(f"Could not read image: {image_path}")
 
-# Ensure image is loaded
-if image_gray is None:
-    raise FileNotFoundError("Error: Could not load image. Check the file path.")
+    # Run inference
+    results = model(image)
 
-# Convert grayscale to 3-channel format
-image = cv2.cvtColor(image_gray, cv2.COLOR_GRAY2BGR)
+    # Process results
+    for result in results:
+        boxes = result.boxes  # Bounding box outputs
+        masks = result.masks  # Segmentation masks
+        keypoints = result.keypoints  # Pose keypoints
+        probs = result.probs  # Classification probabilities
+        obb = result.obb  # Oriented bounding boxes
 
-# Run inference
-results = model(image)
-
-# Process results
-for result in results:
-    boxes = result.boxes  # Bounding box outputs
-    masks = result.masks  # Segmentation masks
-    keypoints = result.keypoints  # Pose keypoints
-    probs = result.probs  # Classification probabilities
-    obb = result.obb  # Oriented bounding boxes
-
-    # Show and save the result
-    result.show()
-    result.save(filename="result.jpg")
+        # Show and save the result
+        result.show()
+        result.save(filename=f"detect_shape/detect_wagons_{filename}.jpg")
+    print("Detection complete.")
+        
+if __name__ == "__main__":
+    detect_wagon("../output_folder_cropped/cropped_49_77_crop_0_1.jpg")
--- src/isolate_color.py
+++ src/isolate_color.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
+import time
 
 # Define the HSV range for different colors
 COLOR_RANGES = {
     "green":  (np.array([40, 50, 50]), np.array([90, 255, 255])),
     "red":    [(np.array([0, 100, 50]), np.array([10, 255, 255])),  # Two ranges for red
                (np.array([170, 100, 50]), np.array([180, 255, 255]))],
-    "black":  (np.array([0, 0, 0]), np.array([180, 255, 50])),
+    "black":  (np.array([0, 0, 0]), np.array([213, 100, 100])),
     "yellow": (np.array([20, 100, 100]), np.array([35, 255, 255])),
     "blue":   (np.array([100, 100, 50]), np.array([130, 255, 255]))
 }
@@ -52,7 +53,12 @@ def draw_contours(image, contours, color_name):
     }
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(image, (x, y), (x + w, y + h), color_bgr.get(color_name, (255, 255, 255)), 3)
+        x1= int((x-(w/2)))
+        y1= int(y-(h/2))
+        width = x + w + 30
+        height = y + h + 30
+        
+        cv2.rectangle(image, (x1,y1), (width, height), color_bgr.get(color_name, (255, 255, 255)), 1)
 
     return image
 
@@ -64,14 +70,18 @@ def main(image_path, track_color):
         raise FileNotFoundError(f"Could not read image: {image_path}")
 
     contours, mask = find_contours(image, track_color)
+    
+    # show mask
+    timestamp_str = str(time.time())
+    cv2.imwrite(f"masks/mask_{timestamp_str}.jpg", mask)
 
     # Draw bounding boxes on the detected objects
     result_image = draw_contours(image, contours, track_color)
 
     # Save the result image
-    cv2.imwrite("result.jpg", result_image)
-
+    filename = image_path.split("/")[-1].split(".")[0]
+    cv2.imwrite(f"detect_colors/{track_color}_{filename}.jpg", result_image)
 
 if __name__ == "__main__":
     # Change the file path and color name as needed
-    main("output_folder_cropped/cropped_49_77_crop_0_0.jpg", "yellow")  # Example: Tracking green wagons
+    main("../output_folder_cropped/cropped_49_77_crop_0_1.jpg", "black")  # Example: Tracking green wagons
